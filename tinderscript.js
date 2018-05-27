7c04d4de8b1b3d9848ca5feb8c7e5116c4f59b52
Michał Jereczek <michaljereczek@gmail.com>
2018-05-27 18:54:11 +0200

    Update tinderscript.js

--- tinderscript.js
+++ tinderscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tinder for Desktop. Automates profile browsing!
 // @namespace    https://greasyfork.org/en/scripts/36887/versions/new
-// @version      1.6
+// @version      1.7
 // @description  When using Tinder on desktop there is need to manually open each new candidates profiles and browse their photos. This script automates it! Just enjoy swaping with just left and right arrows :)
 // @author       https://github.com/trzye
 // @license      Do What The F*ck You Want To Public License
@@ -13,17 +13,38 @@
 // ==/UserScript==
 
 let isOn = true;
-let startupInterval = window.setInterval(startup, 1750);
+let slow = 750
+let fast = 1750
+let actualSpeed = fast
+let startupInterval = window.setInterval(startup, actualSpeed);
 
 function startup(){
     let navElement = document.getElementsByTagName("nav")[0];
     if(navElement != undefined){
-        window.setInterval(tinderscript, 1750);
-        let button = document.createElement("button");
-        button.innerHTML = "Click HERE to turn ON/OFF TinderScript";
-        button.onclick = function(){isOn = !isOn;};
-        navElement.appendChild(button);
-        navElement.insertBefore(button, navElement.firstChild);
+        let tinderScript = window.setInterval(tinderscript, actualSpeed);
+
+        let onOffButton = document.createElement("button");
+        onOffButton.innerHTML = "Click HERE to turn ON/OFF TinderScript";
+        onOffButton.onclick = function(){isOn = !isOn;};
+
+        navElement.appendChild(onOffButton);
+        navElement.insertBefore(onOffButton, navElement.firstChild);
+
+        let speedButton = document.createElement("button");
+        speedButton.innerHTML = "Click HERE to change TinderScript speed";
+        speedButton.onclick = function(){
+            if(actualSpeed == fast) {
+                actualSpeed = slow;
+            } else {
+                actualSpeed = fast;
+            }
+            window.clearInterval(tinderScript);
+            tinderScript = window.setInterval(tinderscript, actualSpeed);
+        };
+
+        navElement.appendChild(speedButton);
+        navElement.insertBefore(speedButton, navElement.firstChild);
+
         window.clearInterval(startupInterval);
     }
 }
